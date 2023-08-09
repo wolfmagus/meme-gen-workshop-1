fa8ad570ef88eb4ffaa205ad00ef4dcd2f868c94
Roy Maegus <wolfmagus0@gmail.com>
2023-08-09 15:54:07 -0400

    image ans spelling update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ export default function Header(){
     return(
         <header>
             <div className='page-title'>
-                <img className='logo-header' src="./images/troll-face.png"></img>
+                <img className='logo-header' src="./images/doje2.png"></img>
                 <h2 className='meme-generator'>
                     Meme Generator
                 </h2>
             </div>
             <div className='page-subtitle'>
-                <h4>React Cource - Project 3</h4>
+                <h4>React Course - Project 3</h4>
             </div>
         </header>
     )
